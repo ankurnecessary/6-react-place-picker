28573818bd7b9e9f3ff0b746f8d0dfb73706ff27
Ankur Gupta <ankurnecessary@gmail.com>
2024-03-27 04:20:48 +1030

    Using async await to fetch places and set the isFetching state of the HTTP request

--- src/components/AvailablePlaces.jsx
+++ src/components/AvailablePlaces.jsx
@@ -3,18 +3,26 @@ import PropTypes from 'prop-types'
 import Places from './Places.jsx'
 
 export default function AvailablePlaces ({ onSelectPlace }) {
+  const [isFetching, setIsFetching] = useState(false)
   const [availablePlaces, setAvailablePlaces] = useState([])
 
   useEffect(() => {
-    fetch('http://localhost:3000/places')
-      .then((response) => response.json())
-      .then((resData) => setAvailablePlaces(resData.places))
+    async function fetchPlaces () {
+      setIsFetching(true)
+      const response = await fetch('http://localhost:3000/places')
+      const resData = await response.json()
+      setAvailablePlaces(resData.places)
+      setIsFetching(false)
+    }
+    fetchPlaces()
   }, [])
 
   return (
     <Places
       title="Available Places"
       places={availablePlaces}
+      isLoading={isFetching}
+      loadingText={'Fetching place data...'}
       fallbackText="No places available."
       onSelectPlace={onSelectPlace}
     />
