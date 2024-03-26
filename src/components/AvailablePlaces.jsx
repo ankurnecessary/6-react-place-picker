454804baa3feda11e7373044174604e0075b0a6d
Ankur Gupta <ankurnecessary@gmail.com>
2024-03-27 03:51:41 +1030

    Added HTTP request to fetch the available places from the backend

--- src/components/AvailablePlaces.jsx
+++ src/components/AvailablePlaces.jsx
@@ -1,12 +1,26 @@
-import Places from './Places.jsx';
+import React, { useState, useEffect } from 'react'
+import PropTypes from 'prop-types'
+import Places from './Places.jsx'
 
-export default function AvailablePlaces({ onSelectPlace }) {
+export default function AvailablePlaces ({ onSelectPlace }) {
+  const [availablePlaces, setAvailablePlaces] = useState([])
+
+  useEffect(() => {
+    fetch('http://localhost:3000/places')
+      .then((response) => response.json())
+      .then((resData) => setAvailablePlaces(resData.places))
+  }, [])
+
   return (
     <Places
       title="Available Places"
-      places={[]}
+      places={availablePlaces}
       fallbackText="No places available."
       onSelectPlace={onSelectPlace}
     />
-  );
+  )
+}
+
+AvailablePlaces.propTypes = {
+  onSelectPlace: PropTypes.func.isRequired
 }
