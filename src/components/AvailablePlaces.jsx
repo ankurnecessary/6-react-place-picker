508b3ccbcf8604d55b448d752c562208e3c72131
Ankur Gupta <ankurnecessary@gmail.com>
2024-03-28 04:04:38 +1030

    Transforming fetched data and sorting it as per the distance from users location

--- src/components/AvailablePlaces.jsx
+++ src/components/AvailablePlaces.jsx
@@ -2,6 +2,7 @@ import React, { useState, useEffect } from 'react'
 import PropTypes from 'prop-types'
 import Places from './Places.jsx'
 import Error from './Error.jsx'
+import { sortPlacesByDistance } from '../loc.js'
 
 export default function AvailablePlaces ({ onSelectPlace }) {
   const [isFetching, setIsFetching] = useState(false)
@@ -19,12 +20,15 @@ export default function AvailablePlaces ({ onSelectPlace }) {
           throw new Error('Failed to fetch response')
         }
 
-        setAvailablePlaces(resData.places)
+        navigator.geolocation.getCurrentPosition((position) => {
+          const sortedPlaces = sortPlacesByDistance(resData.places, position.coords.latitude, position.coords.longitude)
+          setAvailablePlaces(sortedPlaces)
+          setIsFetching(false)
+        })
       } catch (error) {
         setError({ message: error.message || 'Could not fetch places. Please try again later.' })
+        setIsFetching(false)
       }
-
-      setIsFetching(false)
     }
     fetchPlaces()
   }, [])
